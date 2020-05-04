dec0e6e738a7a8cd49eaffdf7803f2f0a75e22d3
Morgan Davies <morgandavies2020@gmail.com>
2020-05-04 22:17:51 +0100

    Added Embed Functionality
    * Utilised on_ready
    * Changed prefix to ?
    * Setup Embeds for ?search

--- bot.py
+++ bot.py
@@ -8,12 +8,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ### GLOBALS ###
+botName = "Discord Dungeon"
 TOKEN = os.getenv('BOT_TOKEN')
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='?')
 client = discord.Client()
 
 ###
-# FUNC NAME: !ping
+# FUNC NAME: on_ready
+# FUNC DESC: Tells you when bot is ready to accept commands
+# FUNC TYPE: Command
+###
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+###
+# FUNC NAME: ?ping
 # FUNC DESC: Pings the bot to check it is live
 # FUNC TYPE: Command
 ###
@@ -27,6 +40,19 @@ async def ping(ctx):
 # FUNC TYPE: Function
 ###
 def throwCodeError(location, code):
+
+    # Construct error embed
+    embed = discord.Embed(
+        colour="#FF0000",
+        title="ERROR", 
+        description="API Request on {} FAILED".format(location)
+    )
+    embed.add_field(name="Status Code", value=code)
+    embed.add_field(name="For more information:", value="See https://www.django-rest-framework.org/api-guide/status-codes/")
+    embed.set_author(name=botName, icon_url="https://i.imgur.com/HxuMICy.jpg")
+
+    # Respond and exit
+    bot.say(embed=embed)
     raise Exception("API Request on {} FAILED.\nStatus code: {}\nSee https://www.django-rest-framework.org/api-guide/status-codes/ for more information".format(location, code))
 
 ###
@@ -46,10 +72,9 @@ def requestAndSearchAPI(query, filteredInput):
         response = None
 
         # Alter query if changing page
-        if (pageNum != 1):
-            newQuery = query + "&page={}".format(str(pageNum))
+        if (pageNum != 1): newQuery = query + "&page={}".format(str(pageNum))
             
-        print("Current query: {}".format(newQuery)) # TODO: Remove this debug
+        print("Current query: {}".format(newQuery))
 
         # API Request
         request = requests.get(newQuery)
@@ -82,17 +107,66 @@ def requestAndSearchAPI(query, filteredInput):
     return output
 
 ###
-# FUNC NAME: !get [ENDPOINT] [ENTITY]
-# FUNC DESC: Queries the Open5e API.
-# ENDPOINT:  A directory can be specified by replacing `ENDPOINT` with the endpoint name (i.e. spells, monsters, etc.). Default is `search`.
+# FUNC NAME: ?search [ENTITY]
+# FUNC DESC: Queries the Open5e search API, basically searches the whole thing for the ENTITY.
 # ENTITY: The DND entity you wish to get infomation on.
 # FUNC TYPE: Command
 ###
-@bot.command(name='get', help='Queries the Open5e API to get the entities infomation.\nUsage: !get [entity]')
-async def get(ctx):
+@bot.command(pass_context=True, name='search', help='Queries the Open5e API to get the entities infomation.\nUsage: ?search [ENTITY]')
+async def search(ctx, *args):
 
-    rawEndpoint = "Spells" # TODO: Find way to get from discord
-    if rawEndpoint == "": rawEndpoint = "search" #TODO: Simulate empty 1st parameter
+    print(args)
+    print(len(args))
+
+    # Verify arg length
+    if len(args) != 1: raise Exception("Command requires only one parameter")
+
+    # Filter input to remove whitespaces and set lowercase
+    filteredInput = args[0].replace(" ", "").lower()
+
+    # Search API
+
+    # TODO: Add notification of query execution
+    matches = requestAndSearchAPI("https://api.open5e.com/search/?format=json", filteredInput)
+
+    # Construct Embed
+    if (matches == []):
+        embed = discord.Embed(
+            colour=0xff0000,
+            title="ERROR", 
+            description="No matches found for {} in the search endpoint".format(filteredInput.upper())
+        )
+        embed.set_author(name=botName, icon_url="https://i.imgur.com/HxuMICy.jpg")
+
+        # await ctx.send(embed) # TODO: Figure out way to send embeds
+        await ctx.send(embed=embed)
+    else:
+        # TODO: Construct embed for correct response
+        print(matches)
+
+###
+# FUNC NAME: ?searchDirectory [DIRECTORY] [ENTITY]
+# FUNC DESC: Queries the Open5e DIRECTORY API.
+# ENDPOINT:  Directory/endpoint name (i.e. spells, monsters, etc.).
+# ENTITY: The DND entity you wish to get infomation on.
+# FUNC TYPE: Command
+###
+@bot.command(name='searchDirectory', help='Queries the Open5e API to get the entities infomation from the specified directory.\nUsage: ?searchDirectory [DIRECTORY] [ENTITY]')
+async def searchDirectory(ctx, *args):
+
+    rawDirectory = ""
+    if len(args) == 1:
+        rawDirectory = "search"
+    elif len(args) == 2:
+        rawDirectory == args[0]
+    else:
+        raise Exception("Command requires at least one parameter")
+
+
+    print(args)
+
+    rawDirectory = "Spels" # TODO: Find way to get from discord
+    if rawDirectory == "": rawDirectory = "search" #TODO: Simulate empty 1st parameter
 
     rawInput = "Wish" # TODO: Find way to get from discord
 
@@ -100,7 +174,7 @@ async def get(ctx):
 
     # Filter input to remove whitespaces and set lowercase
     filteredInput = rawInput.replace(" ", "").lower()
-    filteredEndpoint = rawEndpoint.lower()
+    filteredDirectory = rawDirectory.lower()
 
     # Get Open5e root
     rootQuery = "https://api.open5e.com/?format=json"
@@ -111,17 +185,37 @@ async def get(ctx):
 
     # Iterate through directories, ensure endpoint exists
     rootResponse = rootRequest.json()
-    if filteredEndpoint not in rootResponse.keys(): raise Exception("Endpoint ({}) does not exist or isn't accessible".format(filteredEndpoint))
+    if filteredDirectory not in rootResponse.keys():
+
+        # Construct error embed
+        embed = discord.Embed(
+            colour=255,
+            title="ERROR", 
+            description="Endpoint ({}) does not exist or isn't accessible".format(filteredDirectory)
+        )
+        embed.add_field(name="Available endpoints:", value="")
+        for entity, link in rootResponse.items(): embed.add_field(name=entity, value=link)
+        embed.set_author(name=botName, icon_url="https://i.imgur.com/HxuMICy.jpg")
+
+        await ctx.send(embed=embed) 
+        raise Exception("Endpoint ({}) does not exist or isn't accessible".format(filteredDirectory))
 
     # Search API
-    matches = requestAndSearchAPI("https://api.open5e.com/{}/?format=json".format(filteredEndpoint), filteredInput)
+    matches = requestAndSearchAPI("https://api.open5e.com/{}/?format=json".format(filteredDirectory), filteredInput)
 
-    # TODO: Construct embed here
     if (matches == []):
-        print("No matches found for {} in the {} endpoint".format(filteredInput, filteredEndpoint))
+        embed = discord.Embed(
+            colour="#FF0000",
+            title="ERROR", 
+            description="No matches found for {} in the {} endpoint".format(filteredInput, filteredDirectory),
+        )
+        embed.set_author(name=botName, icon_url="https://i.imgur.com/HxuMICy.jpg")
+
+        await ctx.send(embed) # TODO: Figure out way to send embeds
     else:
         print(matches)
 
     await ctx.send(matches) # TODO: Figure out way to send embeds
+    # await bot.say(embed=embed)
 
 bot.run(TOKEN)
